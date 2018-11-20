eebbbe930590fa72c9c729c520ad006386f89170
Etienne Soucy <esoucy@bigtek.ca>
2018-11-20 11:30:20 -0500

    green: added passing test to check mariadb is enabled and running

--- test/integration/default/test.rb
+++ test/integration/default/test.rb
@@ -8,3 +8,8 @@ describe package('MariaDB-server') do
   it { should be_installed }
   its('version') { (should cmp >= '10.2.7') || (should cmp >= '10.1.23') }
 end
+
+describe service('mariadb') do
+  it { should be_enabled }
+  it { should be_running }
+end
