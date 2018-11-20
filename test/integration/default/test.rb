2a56a9a16b179852d7695b7ef474cb805910dff8
Etienne Soucy <esoucy@bigtek.ca>
2018-11-20 10:49:34 -0500

    red: test mariadb is installed and the proper version

--- test/integration/default/test.rb
+++ test/integration/default/test.rb
@@ -1,3 +1,10 @@
+require 'inspec'
+
 describe file('/etc/hosts') do
   it { should exist }
+end
+
+describe package('MariaDB-server') do
+  it { should be_installed }
+  its('version') { (should cmp >= '10.2.7') || (should cmp >= '10.1.23') }
 end
