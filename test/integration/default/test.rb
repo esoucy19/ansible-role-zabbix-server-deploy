269822cbd1d5aa8616ec98dbe217d1a212f52156
Etienne Soucy <esoucy@bigtek.ca>
2018-11-22 15:13:34 -0500

    red: failed test http

--- test/integration/default/test.rb
+++ test/integration/default/test.rb
@@ -38,3 +38,8 @@ end
 describe port('80') do
   it { should be_listening }
 end
+
+describe http('http://localhost') do
+  its('status') { should cmp 200 }
+  its('title') { should match(/zabbix/) }
+end
