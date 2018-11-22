ac836e6ee9a47f964ebb7d74a6bea13033f66b41
Etienne Soucy <esoucy@bigtek.ca>
2018-11-22 14:28:25 -0500

    green: added passing tests for httpd service and port 80

--- test/integration/default/test.rb
+++ test/integration/default/test.rb
@@ -29,3 +29,12 @@ end
 describe package('zabbix-web') do
   it { should be_installed }
 end
+
+describe service('httpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port('80') do
+  it { should be_listening }
+end
